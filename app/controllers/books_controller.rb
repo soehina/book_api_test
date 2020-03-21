e88dc4c6a9eca47d71d402cd291559c51008e515
soehina <kamuhina@gmail.com>
2020-03-21 18:17:59 +0900

    books_controller パラメータのバリテーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,26 @@
-require "net/http"
-require "uri"
-require "json"
+require 'net/http'
+require 'uri'
+require 'json'
 
 class BooksController < ApplicationController
+    before_action :url_from_keyword
+
+    def results_index
+        @results
+    end
+
     def search
     end
 
     def url_from_keyword
-        keyword = params.permit(:keyword)
-        uri = URI.parse("https://www.googleapis.com/books/v1/volumes?q=#{keyword}&country=JP&maxResults=20")
-        json = Net::HTTP.get(uri)
-        @results = Json.parse(json)
+        if params[:keyword] == ""
+            render action: :search
+        else keyword = params[:keyword]
+            target = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{keyword}&country=JP")
+            uri = URI.parse(target)
+            json = Net::HTTP.get(uri)
+            @results = Json.parse(json)
+            redirect_to(books_results_index_path)
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'books/results_index' => 'books#results_index'
   post 'books/result' => 'books#url_from_keyword'
   root to: 'books#search'
 end
