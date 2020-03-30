607b8d673c72cadae4861b9844bc1776024dbdb3
soehina <kamuhina@gmail.com>
2020-03-31 01:34:54 +0900

    api部分のmodule化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,25 +4,19 @@ require 'json'
 require 'httparty'
 
 class BooksController < ApplicationController
+    include BooksApi
 
     def results_index
         @results = @@results
-        @time = @@time
     end
 
     def search
     end
 
     def url_from_keyword
-        if params[:keyword] == "" || params[:keyword] == nil
-            render action: :search
-        else
-            keyword = params[:keyword]
-            url = URI.encode("https://www.googleapis.com/books/v1/volumes?q=#{params[:keyword]}&country=JP&maxResults=15")
-            response = HTTParty.get(url)
-            @@results = response.parsed_response
-            @@time = @@results["items"].length - 1
-            redirect_to(books_results_index_path)
-        end
+        keyword = params[:keyword]
+        results = BooksApi.get_url(keyword)
+        @@results = results.parsed_response
+        redirect_to(books_results_index_path)
     end
 end
